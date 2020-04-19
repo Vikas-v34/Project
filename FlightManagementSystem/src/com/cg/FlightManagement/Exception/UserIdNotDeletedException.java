ae6d566e47b537162c522e2568bdeff9390ea738
LG <LG@LG-PC>
2020-04-20 00:00:06 +0530

    All User Exception Updated

--- FlightManagementSystem/src/com/cg/FlightManagement/Exception/UserIdNotDeletedException.java
+++ FlightManagementSystem/src/com/cg/FlightManagement/Exception/UserIdNotDeletedException.java
@@ -1,5 +1,21 @@
 package com.cg.FlightManagement.Exception;
 
 public class UserIdNotDeletedException extends Exception {
-
+	private long userId;
+	public UserIdNotDeletedException() {
+		super();
+	}
+	public UserIdNotDeletedException(long userId) {
+		super();
+		this.userId = userId;
+	}
+	@Override
+	public String toString() {
+		return userId + "This User Id does not exist";
+	}
+	@Override
+	public String getMessage() {
+		return userId + "This User Id does not exist";
+	}
+	
 }
--- FlightManagementSystem/src/com/cg/FlightManagement/Exception/UserNotUpdatedException.java
+++ FlightManagementSystem/src/com/cg/FlightManagement/Exception/UserNotUpdatedException.java
@@ -1,5 +1,23 @@
 package com.cg.FlightManagement.Exception;
 
 public class UserNotUpdatedException extends Exception{
-
+	private int user;
+	public UserNotUpdatedException() {
+		super();
+	}
+	public UserNotUpdatedException(int user) {
+		super();
+		this.user = user;
+	}
+	@Override
+	public String toString() {
+		return user + "This User Name does not exist";
+	}
+	@Override
+	public String getMessage() {
+		return user + "This User Name does not exist";
+	}
+	
 }
+
+
