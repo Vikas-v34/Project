0d556ea27188600e77ba729b55c2577b2493c1e4
MohammedThouseef <thouseef31398@gmail.com>
2020-06-12 14:32:02 +0530

    updated

--- FlightManagementSystemPLP/ScheduleService/src/main/java/com/cg/ScheduleService/FlightManagementSystem/Service/ScheduleService.java
+++ FlightManagementSystemPLP/ScheduleService/src/main/java/com/cg/ScheduleService/FlightManagementSystem/Service/ScheduleService.java
@@ -1,14 +1,15 @@
 package com.cg.ScheduleService.FlightManagementSystem.Service;
 
-import java.util.List;
 
-import com.cg.ScheduleService.FlightManagementSystem.Entity.Schedule;
+import org.springframework.stereotype.Service;
 
+
+
+@Service
 public interface ScheduleService {
-	Schedule getByFlightId(long flightid);
+	
 
-	Schedule getByRouteId(long routeid);
-
-	List<Schedule> getAllFlightsandRoutes();
-
+	
+	
+	
 }
