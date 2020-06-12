2c9f80d61866f39b4956e6f706859d45725ca4a3
Vikas-v34 <vikas.vishwakarma15@siesgst.ac.in>
2020-06-12 16:59:04 +0530

    Update

--- FlightManagementSystemPLP/EurekaDiscovery/src/main/java/com/cg/EurekaDiscoveryApplication.java
+++ FlightManagementSystemPLP/EurekaDiscovery/src/main/java/com/cg/EurekaDiscoveryApplication.java
@@ -2,9 +2,11 @@ package com.cg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurekaDiscoveryApplication {
+@EnableEurekaServer
+public class EurekaDiscoveryApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaDiscoveryApplication.class, args);
