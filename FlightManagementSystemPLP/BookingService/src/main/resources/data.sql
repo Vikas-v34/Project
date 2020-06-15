d57c26f8699c51bfc7fbeff0d94c8323e34d5adf
MohammedThouseef <thouseef31398@gmail.com>
2020-06-15 19:32:25 +0530

    Updated

--- FlightManagementSystemPLP/BookingService/src/main/resources/data.sql
+++ FlightManagementSystemPLP/BookingService/src/main/resources/data.sql
@@ -1,7 +1,4 @@
-insert into booking(booking_id,user_id,booking_date,ticket_cost,no_of_passengers,flight_id) values(1001,100,'2020-06-25',5000,4,5001);
-
-insert into booking(booking_id,user_id,booking_date,ticket_cost,no_of_passengers,flight_id) values(1002,101,'2020-06-26',7000,1,5002);
-
-insert into booking(booking_id,user_id,booking_date,ticket_cost,no_of_passengers,flight_id) values(1003,102,'2020-06-27',10000,3,5003);
-
-
+insert into booking(booking_id,user_id,booking_date,ticket_cost,no_of_passengers,flight_id) values(1001,100,'2020-06-25',6000,4,1001);
+insert into booking(booking_id,user_id,booking_date,ticket_cost,no_of_passengers,flight_id) values(1002,101,'2020-06-26',7000,1,1002);
+insert into booking(booking_id,user_id,booking_date,ticket_cost,no_of_passengers,flight_id) values(1003,102,'2020-06-27',10000,3,1003);
+insert into booking(booking_id,user_id,booking_date,ticket_cost,no_of_passengers,flight_id) values(1004,103,'2020-06-29',9000,5,1004);
